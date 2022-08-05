34b3f25791da7ae88b34069832abb2cb39c85441
Dharshuarjun <dhanalakshm77@gmail.com>
2022-08-05 11:27:29 +0530

    saved

--- src/Banner.js
+++ src/Banner.js
@@ -12,7 +12,7 @@ function Banner() {
             <span className="tagline">Welcome to portfolio</span>
             <h1 className="header">{`Hi I'm a webdeveloper`}<span className="wrap">web developer</span></h1>
             
-       <a href="#" className="btn btn-danger p-2-2 m-2">Hire me</a>
+       <a href="#" className="btn btn-danger p-2 m-2">Hire me</a>
             </div>
             <div className="col-xs-12 col-md-6 col-xl-5">
    <img src="../images/mypic.jpg" alt="image" className="rounded-circle"/>
--- src/Skills.js
+++ src/Skills.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3,
+  faJs,
+  faGithub,
+  faReact,
+  faNodeJs,
+} from "@fortawesome/free-brands-svg-icons";
+import { DiMongodb } from "react-icons/di";
 
 function Skills() {
+   
   return (
-   <div>
-    <div className="icon">
-<<FontAwesomeIcon icon="fa-brands fa-html5"/>
+    <section className="myskills">
+        
+    <div className="menu">
+     
+      <li><a href="#"><FontAwesomeIcon icon={faHtml5}/></a>
+      </li>
+     
+      <li><a href="#"><FontAwesomeIcon icon={faCss3}/></a>
+       </li>
+      <li><a href="#"><FontAwesomeIcon icon={faJs} /></a>
+      </li>
+      <li><a href="#"><FontAwesomeIcon icon={faGithub} /></a>
+      </li>
+      <li><a href="#"><FontAwesomeIcon icon={faReact}/></a>
+      </li>
+      <li><a href="#"><FontAwesomeIcon icon={faNodeJs} /></a>
+      </li>
+      <li><a href="#"><DiMongodb classNameName="icon"></DiMongodb></a>
+      </li>
     </div>
-   </div>
-  )
+    </section>
+  );
 }
+// let toggle=document.querySelector(".toggle");
+// let menu=document.querySelector(".menu");
+// toggle.onclick=function(){
+//     menu.classList.toggle("active")
 
-export default Skills
+// }
+export default Skills;
